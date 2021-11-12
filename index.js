3dc2d520e850b3058b92d6ed9a368b7ec348fbfb
kyrian <dr.kyrian.onwe@gmail.com>
2021-11-12 17:36:38 -0600

    commit tree f124526a52fb65ab737814f44a0a1545e715a920

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array){
+    const objId = array.find(element => element.result === "W")
+        if (objId === undefined){
+            return objId;
+        }else {
+            return objId.year;
+        }
+    
+    } 
